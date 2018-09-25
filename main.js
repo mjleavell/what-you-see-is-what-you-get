e13f57d558c94e39eed648da7f257edfc1c61c9e
Maggie Leavell <mjleavell@gmail.com>
2018-09-25 16:11:17 -0500

    completed setup, added arrary

---
+++ main.js
@@ -0,0 +1,32 @@
+let celebs = [
+    {
+        title: "Queen",
+        name: "Elizabeth I",
+        bio: "Queen of England and Ireland. She was the daughter of Henry VIII and Anne Boleyn.",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Darnley_stage_3.jpg/330px-Darnley_stage_3.jpg",
+        lifespan: {
+          birth: 1533,
+          death: 1603
+        }
+    },
+    {
+        title: "Queen",
+        name: "Elizabeth the Queen Mother",
+        bio: "The wife of King George VI. She was Queen consort of the United Kingdom from her husband's accession in 1936 until his death in 1952",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Queen_Elizabeth_the_Queen_Mother_portrait.jpg/330px-Queen_Elizabeth_the_Queen_Mother_portrait.jpg",
+        lifespan: {
+          birth: 1900,
+          death: 2002
+        }
+    },
+    {
+        title: "Princess",
+        name: "Margaret, Countess of Snowdon",
+        bio: "The younger daughter of King George VI and Queen Elizabeth and the only sibling of Queen Elizabeth II",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Princess_Margaret_1965b_%28cropped%29.jpg/330px-Princess_Margaret_1965b_%28cropped%29.jpg",
+        lifespan: {
+          birth: 1930,
+          death: 2002
+        }
+    }
+]
