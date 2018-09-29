483e9252b501437bedc0da048071372f68bef6d1
Maggie Leavell <mjleavell@gmail.com>
2018-09-28 22:32:50 -0500

    updated object. functions working

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
 let celebs = [
     {
-        title: "Queen",
-        name: "Elizabeth I",
-        bio: "Queen of England and Ireland. She was the daughter of Henry VIII and Anne Boleyn.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Darnley_stage_3.jpg/330px-Darnley_stage_3.jpg",
+        title: "Tennis Player",
+        name: "Arthur Ashe",
+        bio: "Arthur Robert Ashe Jr. was an American professional tennis player who won three Grand Slam titles. Ashe was the first black player selected to the United States Davis Cup team and the only black man ever to win the singles title at Wimbledon, the US Open, and the Australian Open. He retired in 1980.",
+        image: "https://thenypost.files.wordpress.com/2018/08/arthur_ashe.jpg?quality=90&strip=all&strip=all",
         lifespan: {
-            birth: 1533,
-            death: 1603
+            birth: 1943,            
+            death: 1993
         }
     },
     {
-        title: "Duchess",
-        name: "Elizabeth the Queen Mother",
-        bio: "The wife of King George VI. She was Queen consort of the United Kingdom from her husband's accession in 1936 until his death.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Queen_Elizabeth_the_Queen_Mother_portrait.jpg/330px-Queen_Elizabeth_the_Queen_Mother_portrait.jpg",
+        title: "Tennis Player",
+        name: "Maria Bueno",
+        bio: "She was a Brazilian professional tennis player. During her career in the 1950s and 1960s, she won 19 Grand Slam titles, making her the most successful South American female tennis player in history, and the only one to ever win Wimbledon.",
+        image: "http://images0.naharnet.com/images/212308/w460.jpg?1528521773",
         lifespan: {
-            birth: 1900,
-            death: 2002
+            birth: 1939,
+            death: 2018
         }
     },
     {
-        title: "Princess",
-        name: "Margaret",
-        bio: "The younger daughter of King George VI and Queen Elizabeth and the only sibling of Queen Elizabeth II.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Princess_Margaret_1965b_%28cropped%29.jpg/330px-Princess_Margaret_1965b_%28cropped%29.jpg",
+        title: "Tennis Player",
+        name: "Maureen Connolly",
+        bio: "She was an American tennis player, the winner of nine Grand Slam singles titles in the early 1950s. In 1953, she became the first woman to win all four Grand Slam tournaments during the same calendar year. ",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Maureen_Connolly_1953.jpg/220px-Maureen_Connolly_1953.jpg",
         lifespan: {
-            birth: 1930,
-            death: 2002
+            birth: 1934,
+            death: 1969
         }
     },
     {
-        title: "King",
-        name: "George VI",
-        bio: "King of the United Kingdom and the Dominions of the British Commonwealth from 11 December 1936 until his death in 1952.",
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/King_George_VI_of_England%2C_formal_photo_portrait%2C_circa_1940-1946.jpg/330px-King_George_VI_of_England%2C_formal_photo_portrait%2C_circa_1940-1946.jpg",
+        title: "Tennis Player",
+        name: "Vitas Gerulaitis",
+        bio: "He was a Lithuanian American professional tennis player. In 1975, Gerulaitis won the men's doubles title at Wimbledon, partnering with Sandy Mayer.During his career Gerulaitis won 25 top-level singles titles and 8 doubles titles.",
+        image: "https://tennistipster.files.wordpress.com/2013/07/standard-vitas-g-2.jpg",
         lifespan: {
-            birth: 1895,
-            death: 1952
+            birth: 1954,
+            death: 1994
         }
     }
 ]
@@ -44,7 +44,6 @@ let celebs = [
 const celebCardEl = document.getElementsByClassName('card');
 const bio = document.getElementsByClassName('cardText');
 const inputText = document.getElementById('editText');
-
 
 const writeToDom = (stringToPrint, elementId) => {
     const selectedDiv = document.getElementById(elementId);
@@ -61,101 +60,58 @@ const celebCardBuilder = () => {
     for (let i = 0; i < celebs.length; i++) {
         let cardIndex = celebs.indexOf(celebs[i]) + 1;
         if (cardIndex % 2) {
-            newString += `<div class="even card" id="${cardIndex}">`
+            newString += `<div class="card even" id="${cardIndex}">`
             newString += `<h3 class="name${i}">${celebs[i].title} ${celebs[i].name}</h3>`
             newString += `<img class="img${i}" style="width: 200px; height: 250px;" src="${celebs[i].image}" alt="Card image cap">`
             newString += `<p class="cardText" id="bio${i}">${celebs[i].bio}</p>`
-            newString += `<p class="life${i}">${celebs[i].lifespan.birth} - ${celebs[i].lifespan.death}</p>`
+            newString += `<p class="card-life">${celebs[i].lifespan.birth} - ${celebs[i].lifespan.death}</p>`
             newString += `</div>`;
             printToDom(newString, "celebCards");
         } else {
-            newString += `<div class="odd card" id="${cardIndex}">`
+            newString += `<div class="card odd" id="${cardIndex}">`
             newString += `<h3 class="name${i}">${celebs[i].title} ${celebs[i].name}</h3>`
             newString += `<img class="img${i}" style="width: 200px; height: 250px;" src="${celebs[i].image}" alt="Card image cap">`
             newString += `<p class="cardText" id="bio${i}">${celebs[i].bio}</p>`
-            newString += `<p class="life${i}">${celebs[i].lifespan.birth} - ${celebs[i].lifespan.death}</p>`
+            newString += `<p class="card-life">${celebs[i].lifespan.birth} - ${celebs[i].lifespan.death}</p>`
             newString += `</div>`;
             printToDom(newString, "celebCards");
         } celebCardClick();
     }
 }
 
-// const celebCardClick = () => {
-//     for (let i = 0; i < celebCardEl.length; i++) {
-//         celebCardEl[i].addEventListener('click', (e) => {
-//             let celebBio = bio[i];
-//             if (e.target.classList.contains('card')) { 
-//                 e.target.classList.toggle('border');
-//                 // e.target.style.border = "4px dotted black";
-//                 document.getElementById('editText').focus();
-//                 updateText(celebBio);
-//             } else {
-//                 // e.target.parentNode.style.border = "4px dotted black";
-//                 e.target.classList.toggle('border');
-//                 document.getElementById('editText').focus();
-//                 updateText(celebBio);
-//             }
-//         })
-//     }
-// }
-
 const celebCardClick = () => {
     removeBorder();
     for (let i = 0; i < celebCardEl.length; i++) {
         celebCardEl[i].addEventListener('click', (e) => {
-            // celebCardEl.classList.remove('border');
-            let celebBio = bio[i];
-            // console.log(e);
-            // removeBorder(e.target.parentNode.children);
-            if (e.currentTarget.classList.contains('card')) {
-                e.currentTarget.classList.toggle('border');
+            if (e.target.classList.contains('card')) {
+                e.target.classList.toggle('border');
                 document.getElementById('editText').focus();
-                // updateText(celebBio);
-                // e.target.style.border = "none";
-                // console.log(e);
-            } else {
-                e.currentTarget.parentNode.classList.toggle('border');
+            } else if (e.target.parentNode.classList.contains('card')){
+                e.target.parentNode.classList.toggle('border');
                 document.getElementById('editText').focus();
-                // updateText(celebBio);
-                // e.target.parentNode.style.border = "none";
-                // console.log(e);
-            } updateText(celebBio);
+            } updateText();
         })
     }
 }
-
-// const removeBorder = (everyCard) => {
-//     let celebCardEl = document.getElementsByClassName('card');
-//     document.getElementById('celebCards')
-//     for (let i = 0; i < celebCardEl.length; i++) {
-//         celebCardEl.addEventListener('click', (e) => {
-//             console.log(e)
-//             if (e.target.classList.contains('border')) {
-//                 e.target.classList.remove('border');
-//             }
-//         })
-//     }
-// }
 
 const removeBorder = () => {
     for (let i = 0; i < celebCardEl.length; i++) {
             celebCardEl[i].classList.remove('border');
         }
 }
-    // if (e.target.classList.contains('card')) {
-    //     e.target.style.border = 'none';
-    // } else {
-    //     e.target.parentNode.style.border = 'none';
-    // }
 
-
-const updateText = (outputText) => {
+const updateText = () => {
     inputText.addEventListener('keyup', (e) => {
         if (e.keyCode === 13) {
             e.target.value = '';
             inputText.blur();
         }else {
-            outputText.innerHTML = e.target.value;
+            for (let i = 0; i < celebCardEl.length; i++) {
+            const celebCard = celebCardEl[i];
+            if (celebCard.classList.contains('border')) {
+                celebCard.childNodes[2].innerHTML = e.target.value;
+            }
+            }
         }
     })
 }
